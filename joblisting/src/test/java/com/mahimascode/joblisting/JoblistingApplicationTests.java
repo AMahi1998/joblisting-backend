2dae4b7edf889a705e1fbb128362ddfe3f23d931
mahimascode <amahima888@gmail.com>
2025-06-19 22:53:43 -0500

    commiting application.properties file changes

---
+++ joblisting/src/test/java/com/mahimascode/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahimascode.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
