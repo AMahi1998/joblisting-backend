b7f52ba82c39f94e55c070985a3312138db8020b
mahimascode <amahima888@gmail.com>
2025-06-24 20:47:52 -0500

    Added Model class for our Attributes

--- joblisting/src/main/java/com/mahimascode/joblisting/JoblistingApplication.java
+++ joblisting/src/main/java/com/mahimascode/joblisting/JoblistingApplication.java
@@ -2,6 +2,15 @@ package com.mahimascode.joblisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -18,6 +27,7 @@ public class JoblistingApplication {
 		final ApiInfoBuilder builder = new ApiInfoBuilder();
 		return builder.build();
 
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JoblistingApplication.class, args);
 	}
